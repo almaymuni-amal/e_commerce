a55d997ca8391f2c90e9c1073d896d5720d7c30e
almaymuni-amal <amalmaymouni9@gmail.com>
2022-01-30 05:13:02 +0300

    cartItem + Router

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Typography , Container , Button , Grid} from '@material-ui/core';
 import useStyles from './styles';
+import CartItem from './CartItem/CartItem';
 
 const Cart = ({cart}) => {
     const classes=useStyles();
-    
+
     if(!cart.line_items)
     return  '...loading';
+    
 
     // just a function returns JSX looks like a subcomponent (easy to call)
     const EmptyCart=() =>(
@@ -20,7 +22,7 @@ const Cart = ({cart}) => {
             <Grid container spacing={3}>
                 {cart.line_items.map( (item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <div>{item.name}</div>
+                        <CartItem item={item}/>
                     </Grid>
                 ))}
             </Grid>
@@ -34,10 +36,12 @@ const Cart = ({cart}) => {
         </>
     )
 
+    
+    
     return (
         <Container>
             <div className='classes.toolbar'/>
-            <Typography className={classes.title} variant='h3'>Your Shopping Cart</Typography>
+            <Typography className={classes.title} variant='h3' gutterBottom>Your Shopping Cart</Typography>
 
             {!cart.line_items.length ? <EmptyCart/> : <FilledCart/>}
         </Container>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,17 +3,20 @@ import {AppBar, Toolbar , IconButton , Badge, MenuItem, Menu , Typography} from 
 import { CallMissedSharp, ShoppingCart } from '@material-ui/icons';
 import useStyles from './styles';
 import Logo from '../../assets/logo.png';
+import {Link , useLocation} from 'react-router-dom'
 
 
 const Navbar = ({allItems}) => {
 
 const classes=useStyles();
+const location = useLocation(); //to hide cart icon if user in cart page
+
     return (
         <div>
             <AppBar position='fixed' className={classes.appBar} color='inherit'>
                 <Toolbar>
                     {/* left */}
-                    <Typography variant='h6' className={classes.title} color='inherit'>
+                    <Typography variant='h6' className={classes.title} color='inherit' component={Link} to='/'>
                         {/* LOGO and Shop name */}
                         <img src={Logo} alt='Maymouni Shop' height='25px' className={classes.image}/>
                         Maymouni Shop
@@ -22,12 +25,18 @@ const classes=useStyles();
                     {/* div for a space  فاصل*/}
                     <div className={classes.grow}/>
                     <div className={classes.button}>
-                        <IconButton aria-label='shopping cart items' color='inherit'>
+                        {/* 1-  <Link to='/cart'></Link> */}
+                        {/* or matrial-ui allows us to do this */}
+                        {/* using && in react : If false , React will ignore and skip it */}
+                        {location.pathname === '/' && 
+                        (
+                        <IconButton component={Link} to='/cart' aria-label='shopping cart items' color='inherit'>
                             {/* badge is the number of items in a shopping cart */}
                             <Badge badgeContent={allItems} color='secondary'>
                                 <ShoppingCart/>
                             </Badge>
                         </IconButton>
+                        )}
                     </div>
                 </Toolbar>
             </AppBar>
